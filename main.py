4381809d7a514834f42b981849530bcbf1b50156
EchoHsiao7 <hsiaoty7@gmail.com>
2024-10-06 19:21:22 -0400

    ff

--- main.py
+++ main.py
@@ -4,48 +4,56 @@ This module contains CRUD database operations.
 
 import sqlite3
 
+
 def connect_db(db_file=".db"):
     connection = sqlite3.connect(db_file)
     return connection
+
 
 def create_table(connection):
     """
     Creates the users table if it doesn't already exist.
     """
-    sql = '''
+    sql = """
     CREATE TABLE IF NOT EXISTS student (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        s_id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL,
         age INTEGER NOT NULL
     );
-    '''
+    """
     connection.execute(sql)
     connection.commit()
+
 
 def insert_student(connection, name, age):
     sql = "INSERT INTO student (name, age) VALUES (?, ?);"
     connection.execute(sql, (name, age))
     connection.commit()
 
-def update_age(connection, id, age):
-    sql = "UPDATE student SET age = ? WHERE id = ?;"
-    connection.execute(sql, (age, id))
+
+def update_age(connection, s_id, age):
+    sql = "UPDATE student SET age = ? WHERE s_id = ?;"
+    connection.execute(sql, (age, s_id))
     connection.commit()
 
-def delete_user(connection, id):
-    sql = "DELETE FROM student WHERE id = ?;"
-    connection.execute(sql, (id,))
+
+def delete_user(connection, s_id):
+    sql = "DELETE FROM student WHERE s_id = ?;"
+    connection.execute(sql, (s_id,))
     connection.commit()
-    
+
+
 def query1(connection):
     sql = "SELECT * FROM student;"
     cursor = connection.execute(sql)
     return cursor.fetchall()
 
-def query2(connection,age):
+
+def query2(connection, age):
     sql = "SELECT * FROM student where age = ?;"
-    cursor = connection.execute(sql,(age,))
+    cursor = connection.execute(sql, (age,))
     return cursor.fetchall()
 
+
 if __name__ == "__main__":
-    conn=connect_db()
+    conn = connect_db()
